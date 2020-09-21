e18f3a27339db266c775f75a41cdd7b8e1f1356a
Shaun <shaun@thenetninja.co.uk>
2020-09-21 12:09:41 +0100

    lesson-8

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
   // data, functions etc
   // template: '<h2>I am the template</h2>',
+  data() {
+    return {
+      title: 'Name of the Wind',
+      author: 'Brandon Sanderson',
+      age: [1,2,3]
+    }
+  }
 })
 
 app.mount('#app')

a863199539aa3ab585fc8b6bde1058de6b0f3a9c
Shaun <shaun@thenetninja.co.uk>
2020-09-21 15:10:11 +0100

    lesson-1

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const app = Vue.createApp({
       showBooks: true,
       title: 'The Way of Kings',
       author: 'Brandon Sanderson',
-      age: 45
+      age: 45,
+      x: 0,
+      y: 0,
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks =  !this.showBooks
+    },
+    handleEvent(e, data) {
+      console.log(e)
+      console.log(data)
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   }
 })

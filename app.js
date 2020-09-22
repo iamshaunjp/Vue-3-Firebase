e9d95dd989abf2e989f51937c84f04f5a3d2f2c8
Shaun <shaun@thenetninja.co.uk>
2020-09-22 13:06:06 +0100

    lesson-14

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const app = Vue.createApp({
   data() {
     return {
-      url: 'https://www.thenetninja.co.uk',
       showBooks: true,
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' },
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
       ],
     }
   },

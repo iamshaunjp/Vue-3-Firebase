8252a289ca62f4d3c1667d6bc67796927f7d7126
Shaun <shaun@thenetninja.co.uk>
2020-09-22 10:21:22 +0100

    lesson-12

--- app.js
+++ app.js
@@ -2,27 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The Way of Kings',
-      author: 'Brandon Sanderson',
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss' },
+        { title: 'the way of kings', author: 'brandon sanderson' },
+        { title: 'something', author: 'something' },
+      ],
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks =  !this.showBooks
     },
-    handleEvent(e, data) {
-      console.log(e.type, e)
-      if (data) {
-        console.log(data)
-      }
-    },
-    handleMousemove(e) {
-      this.x = e.offsetX
-      this.y = e.offsetY
-    }
   }
 })
 

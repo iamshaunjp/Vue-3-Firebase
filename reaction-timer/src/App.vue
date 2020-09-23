d7e0582a76034a9d6541729a51e47cc95d1155aa
Shaun <shaun@thenetninja.co.uk>
2020-09-23 14:10:09 +0100

    lesson-31

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,28 +1,12 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+
 </template>
 
 <script>
-import Block from './components/Block'
-
 export default {
   name: 'App',
-  components: { Block },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-    }
-  },
-  methods: {
-    start() {
-      // set time amount (ms)
-      this.delay = 2000 + Math.random() * 5000
-      this.isPlaying = true
-    },
-  }
+  components: {},
 }
 </script>
 
@@ -34,19 +18,5 @@ export default {
     text-align: center;
     color: #444;
     margin-top: 60px;
-  }
-  button {
-    background: #0faf87;
-    color: white;
-    border: none;
-    padding: 8px 16px;
-    border-radius: 4px;
-    font-size: 16px;
-    letter-spacing: 1px;
-    cursor: pointer;
-  }
-  button[disabled] {
-    opacity: 0.2;
-    cursor: not-allowed;
   }
 </style>

4cf4370851a9bf45d2f57327909d3a0542bf451a
Shaun <shaun@thenetninja.co.uk>
2020-09-23 14:08:45 +0100

    lesson-31

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,21 +1,52 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/Block'
+
 export default {
   name: 'App',
-  components: {}
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      // set time amount (ms)
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    },
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #444;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>

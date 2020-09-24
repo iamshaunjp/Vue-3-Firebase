c8cb56db096ef202f584be294813a660d1516d5c
Shaun <shaun@thenetninja.co.uk>
2020-09-24 14:37:03 +0100

    lesson-37

--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,9 +1,32 @@
 <template>
   <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
 </template>
 
 <script>
 export default {
-  props: ['score']
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers'
+    } else if (this.score < 400) {
+      this.rank = 'Rapid Reflexes'
+    } else {
+      this.rank = '...Snail Pace'
+    }
+  }
 }
 </script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+  }
+</style>

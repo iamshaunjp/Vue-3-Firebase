7f3480bdeca3abd6ed91931b6d60b81ab306832a
Shaun <shaun@thenetninja.co.uk>
2020-09-24 15:38:00 +0100

    lesson-38

--- web-forms/README.md
+++ web-forms/README.md
@@ -1,4 +1,4 @@
-# reaction-timer
+# web-forms
 
 ## Project setup
 ```
---
+++ web-forms/src/components/SignupForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+</style>

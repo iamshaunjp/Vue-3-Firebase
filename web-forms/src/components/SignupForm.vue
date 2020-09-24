387406333095697088e09a4de89ca346e97e547c
Shaun <shaun@thenetninja.co.uk>
2020-09-24 15:45:10 +0100

    lesson-40

--- web-forms/src/components/SignupForm.vue
+++ web-forms/src/components/SignupForm.vue
@@ -5,10 +5,17 @@
 
     <label>Password:</label>
     <input type="password" v-model="password" required>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
-  
+
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
+  <p>Your role: {{ role }}</p>
 </template>
 
 <script>
@@ -17,6 +24,7 @@ export default {
     return {
       email: '',
       password: '',
+      role: 'developer',
     }
   }
 }
@@ -40,7 +48,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;

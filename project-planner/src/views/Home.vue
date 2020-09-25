f478e3d412a6353e95ce94089b050aa13b3cf9a3
Shaun <shaun@thenetninja.co.uk>
2020-09-25 18:44:38 +0100

    lesson-58

--- project-planner/src/views/Home.vue
+++ project-planner/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,17 @@
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err))
+  },
 }
 </script>

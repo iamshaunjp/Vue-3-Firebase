b43732a16693bc8ae01c49734596a8cb337e164f
Shaun <shaun@thenetninja.co.uk>
2020-09-25 13:38:01 +0100

    lesson-50

--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,7 +1,12 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="card">
-    <h2>{{ job.title }}</h2>
+  <div v-for="job in jobs" :key="job.id" class="job">
+    <router-link :to="{name: 'JobDetails', params: { id: job.id }}">
+      <h2>{{ job.title }}</h2>
+    </router-link> 
+    <!-- <router-link :to="`/jobs/${job.id}`">
+      <h2>{{ job.title }}</h2>
+    </router-link>  -->
   </div>
 </template>
 
@@ -20,5 +25,19 @@ export default {
 </script>
 
 <style>
-
+  .job h2{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+  }
+  .job h2:hover {
+    background: #ddd;
+  }
+  .job a{
+    text-decoration: none;
+  }
 </style>

f0f04989602a966bd62abc7c582379ba39784cb0
Shaun <shaun@thenetninja.co.uk>
2020-09-25 13:24:06 +0100

    lesson-49

--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views//jobs/Jobs.vue'
+import JobDetails from '../views//jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/jobs',
     name: 'Jobs',
     component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails
   },
 ]
 
--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,10 +1,16 @@
 <template>
-  Job details
+  <h1>Job Details</h1>
+  <!-- <p>The job id is {{ $route.params.id }}</p> -->
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
 }
 </script>
 

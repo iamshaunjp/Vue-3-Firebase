5e74339e6771fb41ce375b33cfc3dff80ce95857
Shaun <shaun@thenetninja.co.uk>
2020-09-25 16:03:37 +0100

    lesson-55

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Job Details</h1>
+  <h1>{{ job.title }}</h1> <!-- gets an error -->
   <p>The job id is {{ id }}</p>
+  <p></p>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+      .then(res => res.json())
+      .then(data => this.job = data)
+      .catch(err => console.log(err.message))
+  }
 }
 </script>
 
--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -11,12 +11,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-        { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-        { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-      ],
+      jobs: [],
     }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>

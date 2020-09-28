8659a551759a4163810b31771b3309d10772a570
Shaun <shaun@thenetninja.co.uk>
2020-09-28 11:16:39 +0100

    lesson-60

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">delete</span>
+        <span class="material-icons">edit</span>
+        <span class="material-icons tick">done</span>
+      </div>
     </div>
     <div v-if="showDetails" class="details">
       <p>{{ project.details }}</p>
@@ -32,5 +37,19 @@ export default {
   }
   h3 {
     cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
   }
 </style>

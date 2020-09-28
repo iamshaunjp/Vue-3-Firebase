054b72e225f5708436ac481b53d21185181bdd53
Shaun <shaun@thenetninja.co.uk>
2020-09-28 12:21:48 +0100

    lesson-63

--- project-planner/src/router/index.js
+++ project-planner/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import AddProject from '../views/AddProject.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/add',
+    name: 'AddProject',
+    component: AddProject
   },
 ]
 

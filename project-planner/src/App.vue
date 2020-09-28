11abfdcd86c50ef7437fa33bddb731cedc7d0e15
Shaun <shaun@thenetninja.co.uk>
2020-09-28 13:42:27 +0100

    lesson-65

--- project-planner/src/App.vue
+++ project-planner/src/App.vue
@@ -1,6 +1,15 @@
 <template>
+  <Navbar />
   <router-view/>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
 
 <style>
 body {
---
+++ project-planner/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="main-nav">
+    <router-link :to="{ name: 'Home' }">Projects</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add a New Project</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .main-nav {
+    text-align: center;
+    margin: 40px auto;
+  }
+  .main-nav a{
+    display: inline-block;
+    text-decoration: none;
+    margin: 0 10px;
+    color: #999;
+    font-size: 18px;
+  }
+  a.router-link-active {
+    border-bottom: 2px solid #00ce89;
+    padding-bottom: 4px;
+  }
+</style>

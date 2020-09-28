d787eb06aa65797b8639a2e5796cf304624a74d3
Shaun <shaun@thenetninja.co.uk>
2020-09-28 15:08:01 +0100

    lesson-67

--- project-planner/src/views/EditProject.vue
+++ project-planner/src/views/EditProject.vue
@@ -1,14 +1,19 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Title</label>
     <input type="text" v-model="title" required>
     <label>Details</label>
     <textarea v-model="details" required></textarea>
-    <button>Add Project</button>
+    <button>Update Project</button>
   </form>
 </template>
 
 <script>
+// update the project and save the new data to db.json
+//   - create a submit handler and prevent default action
+//   - use the fetch api to send a PATCH request to update
+//   - redirect to the homepage route once done 
+
 export default {
   props: ['id'],
   data() {
@@ -25,6 +30,17 @@ export default {
         this.title = data.title
         this.details = data.details
       }).catch(err => console.log(err))
+  },
+  methods: {
+    handleSubmit() {
+      fetch(this.uri, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: this.title, details: this.details })
+      }).then(() => {
+        this.$router.push('/')
+      }).catch(err => console.log(err))
+    }
   }
 }
 </script>

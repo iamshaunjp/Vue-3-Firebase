dbdf74a04a59ab8df81d3c7092ae5923da32dd2e
Shaun <shaun@thenetninja.co.uk>
2020-09-29 12:57:54 +0100

    lesson-73

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -4,6 +4,7 @@
     <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
     <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 

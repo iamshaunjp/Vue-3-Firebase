e3a92205d372ac163e3ec2ca550033b295ac8f87
Shaun <shaun@thenetninja.co.uk>
2020-10-01 11:11:20 +0100

    lesson-86

--- dojo-blog/src/App.vue
+++ dojo-blog/src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <router-view  />
+
+    <Navbar />
+    <router-view  />
+
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }  
+}
+</script>
 
 <style>
 #app {

75289720eab68c6e06ed5363710fea522ff6c0bc
Shaun <shaun@thenetninja.co.uk>
2020-10-01 13:36:27 +0100

    lesson-89

--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
 import Create from '../views/Create.vue'
+import Tag from '../views/Tag.vue'
 
 const routes = [
   {
@@ -19,6 +20,11 @@ const routes = [
     path: '/create',
     name: 'Create',
     component: Create
+  },
+  {
+    path: '/tags/:tag',
+    name: 'Tag',
+    component: Tag
   }
 ]
 

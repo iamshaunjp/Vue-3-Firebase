1780e37e9b5c6fe0861b5e9dfc997ecaea95bee8
Shaun <shaun@thenetninja.co.uk>
2020-10-01 13:57:17 +0100

    lesson-89

--- dojo-blog/src/composables/getPost.js
+++ dojo-blog/src/composables/getPost.js
@@ -7,11 +7,6 @@ const getPost = (id) => {
 
   const load = async () => {
     try {
-      // simulate delay
-      await new Promise(resolve => {
-        setTimeout(resolve, 2000)
-      })
-
       let data = await fetch('http://localhost:3000/posts/' + id)
       if (!data.ok) {
         throw Error('That post does not exist')

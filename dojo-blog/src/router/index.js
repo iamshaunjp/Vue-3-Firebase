556a7f69b75650a55c150c1474f81a2528cfe29a
Shaun <shaun@thenetninja.co.uk>
2020-10-01 10:10:08 +0100

    lesson-83

--- dojo-blog/src/router/index.js
+++ dojo-blog/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Details from '../views/Details.vue'
+import Create from '../views/Create.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'Details',
     component: Details,
     props: true
+  },
+  {
+    path: '/create',
+    name: Create,
+    component: Create
   }
 ]
 
---
+++ dojo-blog/src/views/Create.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="create">
+    <form>
+      <label>Title:</label>
+      <input v-model="title" type="text" required>
+      <label>Content:</label>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag):</label>
+      <input 
+        @keydown.enter.prevent="handleKeydown" 
+        v-model="tag" 
+        type="text"
+      >
+      <div v-for="tag in tags" :key="tag" class="pill">
+        #{{ tag }}
+      </div>
+      <button>Add Post</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const title = ref('')
+    const body = ref('')
+    const tags = ref([])
+    const tag = ref('')
+
+    const handleKeydown = () => {
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/g,'') // remove all whitespace
+        tags.value.push(tag.value)
+      }
+      tag.value = ''
+    }
+
+    return { body, title, tags, tag, handleKeydown }
+  },
+}
+</script>
+
+<style>
+  form {
+    max-width: 480px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  input, textarea {
+    display: block;
+    margin: 10px 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid #eee;
+  }
+  textarea {
+    height: 160px;
+  }
+  label {
+    display: inline-block;
+    margin-top: 30px;
+    position: relative;
+    font-size: 20px;
+    color: white;
+    margin-bottom: 10px;
+  }
+  label::before {
+    content: "";
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 40px;
+    left: -30px;
+    transform: rotateZ(-1.5deg);
+  }
+  button {
+    display: block;
+    margin-top: 30px;
+    background: #ff8800;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 18px
+  }
+  .pill {
+    display: inline-block;
+    margin: 10px 10px 0 0;
+    color: #444;
+    background: #ddd;
+    padding: 8px;
+    border-radius: 20px;
+    font-size: 14px;
+  }
+</style>

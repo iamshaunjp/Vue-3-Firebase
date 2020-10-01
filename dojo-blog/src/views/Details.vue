3a17625dba47af8a74ca6f37e647b25aa86ca621
Shaun <shaun@thenetninja.co.uk>
2020-10-01 18:27:23 +0100

    lesson-97

--- dojo-blog/src/views/Details.vue
+++ dojo-blog/src/views/Details.vue
@@ -3,6 +3,7 @@
   <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
+    <button @click="handleClick" class="delete">delete post</button>
   </div>
   <div v-else>
     <Spinner />
@@ -10,8 +11,9 @@
 </template>
 
 <script>
-import getPost from '@/composables/getPost'
-import { useRoute } from 'vue-router'
+import getPost from '../composables/getPost'
+import { useRoute, useRouter } from 'vue-router'
+import { projectFirestore } from '../firebase/config'
 
 // component imports
 import Spinner from '../components/Spinner.vue'
@@ -21,13 +23,20 @@ export default {
   components: { Spinner },
   setup(props) {
     const route = useRoute()
-    // console.log(route)
-    // console.log(route.params)
     const { error, post, load } = getPost(route.params.id)
+    const router = useRouter()
 
     load()
 
-    return { error, post }
+    const handleClick = async () => {
+      await projectFirestore.collection('posts')
+        .doc(props.id)
+        .delete()
+
+      router.push('/')
+    }
+
+    return { error, post, handleClick }
   },
 }
 </script>
@@ -44,5 +53,8 @@ export default {
   }
   .pre {
     white-space: pre-wrap;
+  }
+  button.delete {
+    margin: 10px auto;
   }
 </style>

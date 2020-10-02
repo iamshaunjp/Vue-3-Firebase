6512849a9cc4709514b9359bf00359140f5c34ff
Shaun <shaun@thenetninja.co.uk>
2020-10-02 15:41:02 +0100

    lesson-108

--- live-chat/src/components/LoginForm.vue
+++ live-chat/src/components/LoginForm.vue
@@ -2,12 +2,14 @@
   <form @submit.prevent="handleSubmit">
     <input type="email" required placeholder="email" v-model="email">
     <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Log in</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import useLogin from '../composables/useLogin'
 
 export default {
   setup() {
@@ -15,11 +17,16 @@ export default {
     const email = ref('')
     const password = ref('')
 
-    const handleSubmit = () => {
-      console.log(email.value, password.value)
+    const { error, login } = useLogin()
+
+    const handleSubmit = async () => {
+      await login(email.value, password.value)
+      if (!error.value) {
+        console.log('user logged in')
+      }
     }
 
-    return { email, password, handleSubmit }
+    return { email, password, handleSubmit, error }
   }
 }
 </script>

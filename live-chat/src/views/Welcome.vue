a848df1fb472dde52911848fdb2981e5889667bb
Shaun <shaun@thenetninja.co.uk>
2020-10-02 13:32:11 +0100

    lesson-104

--- live-chat/src/views/Welcome.vue
+++ live-chat/src/views/Welcome.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="welcome container">
-    <p>welcome</p>
-    <!-- <SignupForm /> -->
-    <LoginForm />
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm />
+      <p>No account yet? <span @click="showLogin = false">Signup</span> instead.</p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>Already registered? <span @click="showLogin = true">Login</span> instead.</p>
+    </div>
   </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm.vue'
 import LoginForm from '../components/LoginForm.vue'
+import { ref } from 'vue'
 
 export default {
   components: { SignupForm, LoginForm },
+  setup() {
+    const showLogin = ref(true)
+
+    return { showLogin }
+  }
 }
 </script>
 
@@ -37,5 +50,13 @@ export default {
     outline: none;
     color: #999;
     margin: 10px auto;
+  }
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .welcome button {
+    margin: 20px auto;
   }
 </style>

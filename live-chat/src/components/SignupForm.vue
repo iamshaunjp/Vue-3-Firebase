61beb2b2108dc8ce030cd20472af48b15d484f4c
Shaun <shaun@thenetninja.co.uk>
2020-10-02 11:09:22 +0100

    lesson-103

---
+++ live-chat/src/components/SignupForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="display name" v-model="displayName">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>Sign up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // refs
+    const displayName = ref('')
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = () => {
+      console.log(displayName.value, email.value, password.value)
+    }
+
+    return { displayName, email, password, handleSubmit }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- live-chat/src/views/Welcome.vue
+++ live-chat/src/views/Welcome.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="welcome container">
     <p>welcome</p>
+    <SignupForm />
   </div>
 </template>
 
 <script>
-export default {
+import SignupForm from '../components/SignupForm.vue'
 
+export default {
+  components: { SignupForm },
 }
 </script>
 
@@ -14,5 +17,23 @@ export default {
   .welcome {
     text-align: center;
     padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
   }
 </style>

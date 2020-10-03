386ce9eee2a3fdc3e3802218a9bf950d7ad0d56b
Shaun <shaun@thenetninja.co.uk>
2020-10-03 12:16:54 +0100

    lesson-113

--- live-chat/src/router/index.js
+++ live-chat/src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '../firebase/config'
+
+// auth guards
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  console.log('current user in auth guard: ', user)
+  if (!user) {
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
@@ -11,7 +23,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth
   }
 ]
 

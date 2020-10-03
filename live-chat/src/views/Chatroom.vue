60e87de207c554c79de1a1df9f8fdf649a17498a
Shaun <shaun@thenetninja.co.uk>
2020-10-03 12:42:40 +0100

    lesson-115

--- live-chat/src/views/Chatroom.vue
+++ live-chat/src/views/Chatroom.vue
@@ -6,9 +6,23 @@
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import getUser from '../composables/getUser'
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
-  components: { Navbar }
+  components: { Navbar },
+  setup() {
+    const router = useRouter()
+    const { user } = getUser()
+
+    // if the user value is ever null, redirect to welcome screen
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
+  }
 }
 </script>
 

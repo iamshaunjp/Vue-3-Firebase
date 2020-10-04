5a931b67aaded2eb3ff7b0a23dc0f0db5a28b9c5
Shaun <shaun@thenetninja.co.uk>
2020-10-04 10:56:48 +0100

    lesson-118

---
+++ live-chat/src/composables/getCollection.js
@@ -0,0 +1,33 @@
+import { ref } from 'vue';
+import { projectFirestore } from '../firebase/config';
+
+const getCollection = (collection) => {
+
+  let documents = ref(null)
+  let error = ref(null)
+
+  // register the firestore collection reference
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt')
+
+  collectionRef.onSnapshot(snap => {
+    let results = [];
+    snap.docs.forEach(doc => {
+      // must wait for the server to create the timestamp & send it back
+      // we don't want to edit data until it has done this
+      doc.data().createdAt && results.push({...doc.data(), id: doc.id})
+    });
+    
+    // update values
+    documents.value = results;
+    error.value = null;
+  }, err => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'could not fetch the data'
+  })
+
+  return { error, documents }
+}
+
+export default getCollection

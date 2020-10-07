d8aed9f0c620679ab15dd3954ba97182edbac215
Shaun <shaun@thenetninja.co.uk>
2020-10-07 13:51:06 +0100

    lesson-133

---
+++ muso-ninjas/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="content">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+  .content {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 20px;
+  }
+</style>
---
+++ muso-ninjas/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <p>Homepage</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+}
+</script>

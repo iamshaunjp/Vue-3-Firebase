1c030cd50f797e89a6b66e5d45655bf3f15f593a
Shaun <shaun@thenetninja.co.uk>
2020-10-08 15:45:59 +0100

    lesson-141

--- muso-ninjas/src/App.vue
+++ muso-ninjas/src/App.vue
@@ -1,8 +1,17 @@
 <template>
+  <Navbar />
   <div class="content">
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: { Navbar }  
+}
+</script>
 
 <style>
   .content {
---
+++ muso-ninjas/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png"/>
+      <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <button>Logout</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .navbar {
+    padding: 16px 10px;
+    margin-bottom: 60px;
+    background: white;
+  }
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  nav img {
+    max-height: 60px;
+  }
+  nav h1 {
+    margin-left: 20px;
+  }
+  nav .links {
+    margin-left: auto;
+  }
+  nav .links a, button {
+    margin-left: 16px;
+    font-size: 14px;
+  }
+</style>

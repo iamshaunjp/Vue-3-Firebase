334bd8a88d2097e74947e7258791d57b62780939
Shaun <shaun@thenetninja.co.uk>
2020-10-09 12:33:21 +0100

    lesson-151

--- muso-ninjas/src/views/Home.vue
+++ muso-ninjas/src/views/Home.vue
@@ -2,20 +2,18 @@
   <div class="home">
     <div v-if="error" class="error">Could not fetch the data</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <ListView :playlists="documents" />
     </div>
   </div>
 </template>
 
 <script>
-// challenge
-//    - get realtime data from the playlists collection
-//    - output 2 conditional items - error, or playlist titles
-
+import ListView from '../components/ListView.vue'
 import getCollection from '../composables/getCollection'
 
 export default {
   name: 'Home',
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection('playlists')
 

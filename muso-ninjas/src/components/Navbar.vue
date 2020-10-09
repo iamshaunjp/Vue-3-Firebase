9d4e756df9eecbd5f5cb5634980618d11a7a8a65
Shaun <shaun@thenetninja.co.uk>
2020-10-09 12:37:59 +0100

    lesson-152

--- muso-ninjas/src/components/Navbar.vue
+++ muso-ninjas/src/components/Navbar.vue
@@ -5,6 +5,7 @@
       <h1><router-link :to="{ name: 'Home' }">Muso Ninjas</router-link></h1>
       <div class="links">
         <div v-if="user">
+          <router-link :to="{ name: 'CreatePlaylist' }">Create Playlist</router-link>
           <button @click="handleClick">Logout</button>
         </div>
         <div v-else>
--- muso-ninjas/src/router/index.js
+++ muso-ninjas/src/router/index.js
@@ -4,11 +4,24 @@ import Login from '../views/auth/Login.vue'
 import Signup from '../views/auth/Signup.vue'
 import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
 
+// route guard
+import { projectAuth } from '../firebase/config';
+
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  if (!user) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
+}
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    beforeEnter: requireAuth
   },
   {
     path: '/login',
@@ -23,7 +36,8 @@ const routes = [
   {
     path: '/playlists/create',
     name: 'CreatePlaylist',
-    component: CreatePlaylist
+    component: CreatePlaylist,
+    beforeEnter: requireAuth
   }
 ]
 

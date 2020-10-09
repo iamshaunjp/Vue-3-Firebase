44f275210bfd7222cf0440fbabfa62a4b6c9e3d7
Shaun <shaun@thenetninja.co.uk>
2020-10-09 10:19:29 +0100

    lesson-147

---
+++ muso-ninjas/src/composables/useStorage.js
@@ -0,0 +1,28 @@
+import { projectStorage } from '../firebase/config'
+import { ref } from "vue"
+import getUser from "./getUser"
+
+const { user } = getUser()
+
+const useStorage = () => {
+  const error = ref(null)
+  const url = ref(null)
+  const filePath = ref(null)
+
+  const uploadImage = async (file) => {
+    filePath.value = `covers/${user.value.uid}/${file.name}`
+    const storageRef = projectStorage.ref(filePath.value)
+
+    try {
+      const res = await storageRef.put(file)
+      url.value = await res.ref.getDownloadURL()
+    } catch (err) {
+      console.log(err.message)
+      error.value = err
+    }
+  }
+
+  return { uploadImage, url, filePath, error }
+}
+
+export default useStorage;

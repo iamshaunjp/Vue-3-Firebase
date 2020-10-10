dbaaaf298eef47488e44e2de43ac8413f3dea572
Shaun <shaun@thenetninja.co.uk>
2020-10-10 13:25:50 +0100

    lesson-169

--- muso-ninjas/src/components/Navbar.vue
+++ muso-ninjas/src/components/Navbar.vue
@@ -7,6 +7,7 @@
         <div v-if="user">
           <router-link :to="{ name: 'CreatePlaylist' }">Create Playlist</router-link>
           <router-link :to="{ name: 'UserPlaylists' }">My Playlists</router-link>
+          <span>Hi there, {{ user.displayName }}</span>
           <button @click="handleClick">Logout</button>
         </div>
         <div v-else>
@@ -69,5 +70,12 @@ export default {
   nav .links a, button {
     margin-left: 16px;
     font-size: 14px;
+  }
+  span {
+    font-size: 14px;
+    display: inline-block;
+    margin-left: 16px;
+    padding-left: 16px;
+    border-left: 1px solid #eee;
   }
 </style>

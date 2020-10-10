e33d11fa6d3f7ea52141d5b956e51a87aa590d70
Shaun <shaun@thenetninja.co.uk>
2020-10-10 11:24:29 +0100

    lesson-164

--- muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -15,7 +15,14 @@
 
     <!-- song list -->
     <div class="song-list">
-      <p>song list here</p>
+      <div v-if="!playlist.songs.length">No songs have been added to this playlist yet.</div>
+      <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+        <div class="details">
+          <h3>{{ song.title }}</h3>
+          <p>{{ song.artist }}</p>
+        </div>
+        <button v-if="ownership">delete</button>
+      </div>
       <AddSong :playlist="playlist" />
     </div>
     
@@ -96,5 +103,13 @@ export default {
   }
   .description {
     text-align: left;
+  }
+  .single-song {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px dashed var(--secondary);
+    margin-bottom: 20px;
   }
 </style>

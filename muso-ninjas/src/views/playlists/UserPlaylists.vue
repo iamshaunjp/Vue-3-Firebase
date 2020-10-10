672c0373a33821e4ca1938d121e7f29340271719
Shaun <shaun@thenetninja.co.uk>
2020-10-10 13:19:43 +0100

    lesson-168

--- muso-ninjas/src/views/playlists/UserPlaylists.vue
+++ muso-ninjas/src/views/playlists/UserPlaylists.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="user-playlists">
     <h2>My Playlists</h2>
+    <div v-if="playlists">
+      <ListView :playlists="playlists" />
+    </div>
+    <router-link :to="{ name: 'CreatePlaylist' }" class="btn">Create a New Playlist</router-link>
   </div>
 </template>
 
 <script>
 import getUser from '@/composables/getUser'
 import getCollection from '@/composables/getCollection'
+import ListView from '@/components/ListView.vue'
 
 export default {
+  components: { ListView },
   setup() {
     const { user } = getUser()
     const { documents: playlists } = getCollection(

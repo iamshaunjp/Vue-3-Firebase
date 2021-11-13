9bcf11a9437b89ff7b29065c24bb563319697111
iamshaunjp <shaun@thenetninja.co.uk>
2021-11-13 15:32:16 +0000

    lesson-173

---
+++ reading-list/src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCThpQsGd_8rUwGd-t9qCmYN4x3e_uVbVs",
+  authDomain: "vue-3-fb9.firebaseapp.com",
+  projectId: "vue-3-fb9",
+  storageBucket: "vue-3-fb9.appspot.com",
+  messagingSenderId: "544877178259",
+  appId: "1:544877178259:web:78d7ae682ca4ea36224c66"
+}
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+export { db }

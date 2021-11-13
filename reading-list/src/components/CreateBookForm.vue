372acc1ea6a1f258885f41915bb0c5b5993a18f4
iamshaunjp <shaun@thenetninja.co.uk>
2021-11-13 18:06:35 +0000

    lesson-176

--- reading-list/src/components/CreateBookForm.vue
+++ reading-list/src/components/CreateBookForm.vue
@@ -1,13 +1,52 @@
 <template>
-  <div></div>
+  <form @submit.prevent="handleSubmit">
+    <h3>Add a New Book</h3>
+
+    <label for="title">Book title:</label>
+    <input type="text" name="title" v-model="title" required>
+
+    <label for="author">Book author:</label>
+    <input type="text" name="author" v-model="author" required>
+
+    <button>Add Book</button>
+  </form>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
 
+// firebase imports
+import { db } from '../firebase/config'
+import { addDoc, collection } from 'firebase/firestore'
+
+export default {
+  setup() {
+    const title = ref('')
+    const author = ref('')
+
+    const handleSubmit = async () => {
+      const colRef = collection(db, 'books')
+
+      await addDoc(colRef, { 
+        title: title.value, 
+        author: author.value,
+        isFav: false
+      })
+
+      // reset the form
+      title.value = ''
+      author.value = ''
+    }
+
+    return { handleSubmit, title, author }
+  }
 }
 </script>
 
 <style>
-
+form {
+  padding: 10px;
+  margin-top: 10px;
+  border: 1px dashed #c3c8ce;
+}
 </style>

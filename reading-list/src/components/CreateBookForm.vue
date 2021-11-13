a4cfc7e6951c629b2128af8438d3523aeb0d8415
iamshaunjp <shaun@thenetninja.co.uk>
2021-11-13 18:13:03 +0000

    lesson-175

--- reading-list/src/components/CreateBookForm.vue
+++ reading-list/src/components/CreateBookForm.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <form @submit.prevent="handleSubmit">
+    <h3>Add a New Book</h3>
+
+    <label for="title">Book title:</label>
+    <input type="text" name="title" v-model="title" required>
+
+    <label for="author">Book author:</label>
+    <input type="text" name="author" v-model="author" required>
+
+    <button>Add Book</button>
+  </form>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
 
+export default {
+  setup() {
+    const title = ref('')
+    const author = ref('')
+
+    const handleSubmit = async () => {
+      console.log(title.value, author.value)
+    }
+
+    return { handleSubmit, title, author }
+  }
 }
 </script>
 
 <style>
-
+form {
+  padding: 10px;
+  margin-top: 10px;
+  border: 1px dashed #c3c8ce;
+}
 </style>

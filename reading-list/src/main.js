d35e5491e0463b2d3b53aab8f97a7b2046722f0a
iamshaunjp <shaun@thenetninja.co.uk>
2021-11-14 09:58:34 +0000

    lesson-185

--- reading-list/src/main.js
+++ reading-list/src/main.js
@@ -2,4 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// firebase imports
+import { auth } from './firebase/config'
+import { onAuthStateChanged } from 'firebase/auth'
+
+let app
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App).use(router).mount('#app')
+  }
+})
